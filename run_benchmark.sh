a10dd43185888b78644508104367e2707f72aad3
Tarek Ramadan <tramadan.up@gmail.com>
2021-02-04 17:38:03 +0100

    cleanup

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Variables
-#HRZN=8
-#${HRZN}
+HRZN=8
+
 # generating plan for robot/order with id = 1
-clingo $@ --outf=0 -V0 --out-atomf=%s. --quiet=1,2,2 -c horizon=2 -c rid=1 -c oid=1 asprilo-encodings/m/merge/encoding/encoding-m.lp lab/instance.lp | head -n1 > lab/individual_plans/plan_r1.lp
+clingo $@ --outf=0 -V0 --out-atomf=%s. --quiet=1,2,2 -c horizon=${HRZN} -c rid=1 -c oid=1 asprilo-encodings/m/merge/encoding/encoding-m.lp lab/instance.lp | head -n1 > lab/individual_plans/plan_r1.lp
 
 # generating plan for robot/order with id = 2
-clingo $@ --outf=0 -V0 --out-atomf=%s. --quiet=1,2,2 -c horizon=4 -c rid=2 -c oid=2 asprilo-encodings/m/merge/encoding/encoding-m.lp lab/instance.lp | head -n1 > lab/individual_plans/plan_r2.lp
+clingo $@ --outf=0 -V0 --out-atomf=%s. --quiet=1,2,2 -c horizon=${HRZN} -c rid=2 -c oid=2 asprilo-encodings/m/merge/encoding/encoding-m.lp lab/instance.lp | head -n1 > lab/individual_plans/plan_r2.lp
 
 # merging individual plans
 clingo $@ --outf=0 -V0 --out-atomf=%s. --quiet=1,2,2 asprilo-encodings/m/merge/merge.lp lab/individual_plans/{plan_r1.lp,plan_r2.lp} | head -n1 > lab/merged_plan.lp
